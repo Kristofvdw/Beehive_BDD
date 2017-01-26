e60be97158867541549196f0a11cf2484cc4e8a9
vdwJasper <jasper_vdw@live.be>
2017-01-26 08:41:18 +0100

    gitignore update

--- src/main/java/com/refleqt/jbc/support/DriverProvider.java
+++ src/main/java/com/refleqt/jbc/support/DriverProvider.java
@@ -21,8 +21,7 @@ public class DriverProvider {
     public static WebDriver setupDriver() {
         int implicitWait = 60;
         //Create chromeDriver
-        String username = System.getProperty("user.name");
-        String path = "C:\\Users\\" + username + "\\Dropbox\\Beehive\\SWM Taken\\Testing\\chromedriver.exe";
+        String path = "C:\\Users\\jaspe\\Dropbox\\Beehive\\SWM Taken\\Testing\\chromedriver.exe";
         System.setProperty("webdriver.chrome.driver", path);
         driver = new ChromeDriver(createLocalCapabilities());
         //Maximize screen
